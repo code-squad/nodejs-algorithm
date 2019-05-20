3ce717aacd382d4a0f0ca5d6661d0c3c863acb71
Ruby Lee <48516485+ruby413@users.noreply.github.com>
2019-05-21 00:16:46 +0900

    [Ruby] 190520 알고리즘 pullRequest 합니다 (#2)
    * Add 190521 problem
    
    프로그래머스 k번째수
    
    * programmers runner
    
    * programmers runner

---
+++ 190520/ruby/runner.js
@@ -0,0 +1,12 @@
+
+const solution = (participant, completion) => {
+    let partiPeople = participant.sort();
+    let completePeople = completion.sort();
+    for ( let i = 0; i < partiPeople.length; i++ ) {
+        if ( partiPeople[i] !== completePeople[i] ) {
+            return partiPeople[i];
+        }
+    }
+}
+
+console.log( solution(["mislav", "stanko", "mislav", "ana"],["stanko", "ana", "mislav"]) )
