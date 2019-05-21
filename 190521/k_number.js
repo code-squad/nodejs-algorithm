7843a662cb89795713c454e90b86d84b70654a9a
LeeJuyeon <ruby413@hanmail.net>
2019-05-21 11:06:33 +0900

    k번째 수 문제풀이 -map 사용해서 slice 로 answer 에 push

--- 190521/k_number.js
+++ 190521/k_number.js
@@ -1,5 +1,15 @@
 const solution = (arr, commands) => {
+<<<<<<< HEAD
     let answer = commands.map( el => arr.slice(el[0]-1,el[1]).sort(sortFuc)[el[2]-1])
+=======
+    const answer = [];
+    const first = commands.map( el => el[0]-1 );
+    const second = commands.map( el => el[1] );
+    const third = commands.map( el => el[2]-1 );
+    for ( let i = 0; i <  first.length; i++ ){
+        answer.push(arr.slice(first[i],second[i]).sort(sortFuc)[third[i]]);
+    }
+>>>>>>> k번째 수 문제풀이
     return answer;
 }
 
