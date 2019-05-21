5343f71fb957aa1ee3a263a6c79401321f8507ee
sonypark <sonypark0204@gmail.com>
2019-05-21 10:24:21 +0900

    [프로그래머스] k번째 수 풀이
    - 사용언어: 파이썬

--- 190521/sony/kth_number.py
+++ 190521/sony/kth_number.py
@@ -1,3 +1,22 @@
+
+
+def solution(array,commands):
+    answer = []
+    for i in range(len(commands)):
+        start = commands[i][0]
+        end = commands[i][1]
+        kth = commands[i][2]
+        a = array[start-1:end]
+        a.sort()
+        answer.append(a[kth-1])
+    return answer
+
+arr = [1, 5, 2, 6, 3, 7, 4]
+commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+print(solution(arr,commands))
+
+
+
 def solution(array, commands):
     answer = []
     for c in commands:
