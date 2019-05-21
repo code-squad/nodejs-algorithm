9cbcf32b57156f0f054acd48c7b22471e329452e
mukeunzi <eunji057@gmail.com>
2019-05-21 11:22:48 +0900

    Programmers 완주하지 못한 선수 문제풀이
    - 정렬 문제풀이

---
+++ 190520/mukeunzi/NotFinishedPlayer.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class NotFinishedPlayer {
+	
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        for(int i=0;i<completion.length;i++) {
+        	if(!participant[i].equals(completion[i])) {
+        		return participant[i];
+        	}
+        }
+        
+        return participant[participant.length-1];
+    }
+
+}
