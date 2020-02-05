a8b816400484fa8bd3caa7c24ab18c92fa14e51f
IdionKim <ksundong@gmail.com>
2020-02-05 10:06:07 +0900

    프로그래머스 가운데 글자 가져오기 풀이 (#91)

---
+++ 200204/ksundong/GetMiddleCharacterSolutionTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+    Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void testSolution1() {
+        String input = "abcde";
+        String expected = "c";
+        String actual = solution.solution(input);
+        assertEquals(expected, actual, "대상 글자는 가운데 글자가 아닙니다.");
+    }
+
+    @Test
+    void testSolution2() {
+        String input = "qwer";
+        String expected = "we";
+        String actual = solution.solution(input);
+        assertEquals(expected, actual, "대상 글자는 가운데 글자가 아닙니다.");
+    }
+}
