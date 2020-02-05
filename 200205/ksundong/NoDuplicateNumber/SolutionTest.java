fa1c4f6ffda7f7ebd05a1411b18b97ccf9f2c390
IdionKim <ksundong@gmail.com>
2020-02-05 23:25:31 +0900

    프로그래머스 같은 숫자는 싫어 풀이

---
+++ 200205/ksundong/NoDuplicateNumber/SolutionTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class SolutionTest {
+    Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void testSolution1() {
+        int[] input = {1, 1, 3, 3, 0, 1, 1};
+        int[] expected = {1, 3, 0, 1};
+        int[] actual = solution.solution(input);
+        assertArrayEquals(expected, actual, "에상한 배열과 일치하지 않습니다.");
+    }
+
+    @Test
+    void testSolution2() {
+        int[] input = {4, 4, 4, 3, 3};
+        int[] expected = {4, 3};
+        int[] actual = solution.solution(input);
+        assertArrayEquals(expected, actual, "에상한 배열과 일치하지 않습니다.");
+    }
+}
